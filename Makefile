2a2d273205528bb20ae55daecbf1f65100a2edf3
Nikolai Markov <frog158@gmail.com>
2022-04-11 16:48:56 +0300

    first commit

---
+++ Makefile
@@ -0,0 +1,7 @@
+build:
+	poetry build
+install:
+	python3 -m pip install --user dist/hexlet_code-0.1.0-py3-none-any.whl
+uninstall:
+	python3 -m pip uninstall dist/hexlet_code-0.1.0-py3-none-any.whl
+	
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,13 @@
+import argparse
+from sqlite3 import paramstyle
+
+
+def main():
+    pharser = argparse.ArgumentParser(description="Generate diff")
+    pharser.add_argument("first_file")
+    pharser.add_argument("second_file")
+    pharser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
