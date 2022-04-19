2483d2a64a77a9761fde1e894457f30fca0ba804
Nikolai Markov <frog158@gmail.com>
2022-04-19 10:10:28 +0300

    first iteration

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/frog158/python-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/frog158/python-project-lvl2/actions)
+###First iteration
+[![asciicast](https://asciinema.org/a/CygD0q3E1ZPwEdCm97rMmpFun.svg)](https://asciinema.org/a/CygD0q3E1ZPwEdCm97rMmpFun)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,13 +1,41 @@
 import argparse
-from sqlite3 import paramstyle
+import json
+from ntpath import join
+
+def generate_diff(first_file, second_file):
+    first_json = json.load(open(first_file))
+    second_json = json.load(open(second_file))
+    all_keys = set(filter(None, first_json)) | set(filter(None, second_json))
+    all_keys = sorted(all_keys)
+    result = []
+    result.append("{")
+    for key in all_keys:
+        if first_json.get(key) is not None and second_json.get(key) is not None:
+            if first_json.get(key) == second_json.get(key):
+                result.append(f"    {key}: {first_json[key]}")
+            else:
+                result.append(f"  - {key}: {first_json[key]}")
+                result.append(f"  + {key}: {second_json[key]}")
+        else:
+            if first_json.get(key) is not None:
+                result.append(f"  - {key}: {first_json[key]}")
+            else:
+                result.append(f"  + {key}: {second_json[key]}")
+    result.append("}")
+    return "\n".join(result)
+
+
 
 
 def main():
     pharser = argparse.ArgumentParser(description="Generate diff")
-    pharser.add_argument("first_file")
-    pharser.add_argument("second_file")
+    pharser.add_argument("first_file", type=str, help="")
+    pharser.add_argument("second_file", type=str, help="")
     pharser.add_argument("-f", "--format")
-    pharser.parse_args()
+    args = pharser.parse_args()
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
+
 
 
 if __name__ == "__main__":
